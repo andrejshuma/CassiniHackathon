bb1df532f80d48022055402f340d92ad2ac58f19
DavorAngelov <ddavor.angelov@gmail.com>
2025-05-17 11:04:35 +0200

    homepage start

--- preventiq/src/pages/Home/Home.jsx
+++ preventiq/src/pages/Home/Home.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import Navbar from "../GeneralComponents/Navbar";
-
+import { Bell } from 'lucide-react';
+import userDefault from "../../assets/userDefault.jpg";
 const Home = () => {
-  return (
-    <>
-      <Navbar />
-      <h1>Home</h1>
-    </>
-  );
+    return (
+        <>
+            <Navbar/>
+            {/* User Header */}
+            <div className="px-6 py-4 flex justify-between items-center" data-theme = "nord">
+                <div className="flex items-center space-x-3">
+                    <div
+                        className="w-12 h-12 rounded-full bg-primary-content flex items-center justify-center overflow-hidden">
+                        <img
+                            src={userDefault}
+                            alt="User avatar"
+                            className="w-full h-full object-cover"
+                        />
+                    </div>
+                    <div className="font-semibold text-lg">Hello, Ana!</div>
+                </div>
+            </div>
+
+
+        </>
+    );
 };
 
 export default Home;
