e4f18a48fcbd36cbec2401a99cdc977d5432bfec
DavorAngelov <ddavor.angelov@gmail.com>
2025-05-17 12:04:01 +0200

    second phase of homepage

--- preventiq/src/pages/Home/Home.jsx
+++ preventiq/src/pages/Home/Home.jsx
@@ -1,31 +1,23 @@
 import React from "react";
 import Navbar from "../GeneralComponents/Navbar";
 import { Bell } from 'lucide-react';
-import userDefault from "../../assets/userDefault.jpg";
 import Slider_features from "./Slider_features.jsx";
+
+import Top from "./HomepageView/Top.jsx";
+import Metrics from "./HomepageView/Metrics.jsx";
+
 
 const Home = () => {
     const [subPage, setSubPage] = React.useState("home"); // home, pollen, uv, pollution, mental
 
     return (
         <>
-            <Navbar />
-            <h1>Home</h1>
-            <Slider_features activeSubpage={subPage} changeSubpage={setSubPage} />
+
             {/* User Header */}
-            <div className="px-6 py-4 flex justify-between items-center" data-theme = "nord">
-                <div className="flex items-center space-x-3">
-                    <div
-                        className="w-12 h-12 rounded-full bg-primary-content flex items-center justify-center overflow-hidden">
-                        <img
-                            src={userDefault}
-                            alt="User avatar"
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
-                    <div className="font-semibold text-lg">Hello, Ana!</div>
-                </div>
-            </div>
+
+            <Top/>
+            <Slider_features activeSubpage={subPage} changeSubpage={setSubPage} />
+            <Metrics/>
         </>
     );
 };
