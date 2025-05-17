113fd66c2aa7dedd69cb8dddc1c2f974ef87c523
Borjan Gjorgjievski <borjangjorgjievski1@gmail.com>
2025-05-17 12:52:39 +0200

    feature commit

--- preventiq/src/pages/Home/Home.jsx
+++ preventiq/src/pages/Home/Home.jsx
@@ -3,8 +3,10 @@ import Navbar from "../GeneralComponents/Navbar";
 import { Bell } from 'lucide-react';
 import userDefault from "../../assets/userDefault.jpg";
 import Slider_features from "./Slider_features.jsx";
-import FeaturePages from "./FeaturePages.jsx";
+import FeaturePages from "./FeatureSubpages.jsx";
 import {pollenData, uvData, pollutionData} from "./data.js";
+import { pollenAdvice, pollutionAdvice, uvAdvice } from "./advices.js";
+import Tips from "./Tips.jsx";
 
 const Home = () => {
     const [subPage, setSubPage] = React.useState("pollen"); // home, pollen, uv, pollution, mental
@@ -13,6 +15,11 @@ const Home = () => {
         "uv": uvData,
         "pollution": pollutionData,
     }
+    const getTips = {
+        "pollen": pollenAdvice,
+        "uv": uvAdvice,
+        "pollution": pollutionAdvice,
+    }
     return (
         <>
             <Navbar />
@@ -20,6 +27,7 @@ const Home = () => {
             <Slider_features activeSubpage={subPage} changeSubpage={setSubPage} />
             {/* User Header */}
             { subPage === "home" ? null : <FeaturePages data={getData[subPage]} dataName={subPage}/> }
+            { subPage === "home" ? null : <Tips tips={getTips[subPage]}/> }
         </>
     );
 };
