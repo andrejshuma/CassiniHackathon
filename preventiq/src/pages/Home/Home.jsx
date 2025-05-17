cee8353f6f71889b42e3357f32a3ca48ff401430
Borjan Gjorgjievski <borjangjorgjievski1@gmail.com>
2025-05-17 11:51:19 +0200

    Added feature chart bars

--- preventiq/src/pages/Home/Home.jsx
+++ preventiq/src/pages/Home/Home.jsx
@@ -3,29 +3,23 @@ import Navbar from "../GeneralComponents/Navbar";
 import { Bell } from 'lucide-react';
 import userDefault from "../../assets/userDefault.jpg";
 import Slider_features from "./Slider_features.jsx";
+import FeaturePages from "./FeaturePages.jsx";
+import {pollenData, uvData, pollutionData} from "./data.js";
 
 const Home = () => {
-    const [subPage, setSubPage] = React.useState("home"); // home, pollen, uv, pollution, mental
-
+    const [subPage, setSubPage] = React.useState("pollen"); // home, pollen, uv, pollution, mental
+    const getData = {
+        "pollen": pollenData,
+        "uv": uvData,
+        "pollution": pollutionData,
+    }
     return (
         <>
             <Navbar />
             <h1>Home</h1>
             <Slider_features activeSubpage={subPage} changeSubpage={setSubPage} />
             {/* User Header */}
-            <div className="px-6 py-4 flex justify-between items-center" data-theme = "nord">
-                <div className="flex items-center space-x-3">
-                    <div
-                        className="w-12 h-12 rounded-full bg-primary-content flex items-center justify-center overflow-hidden">
-                        <img
-                            src={userDefault}
-                            alt="User avatar"
-                            className="w-full h-full object-cover"
-                        />
-                    </div>
-                    <div className="font-semibold text-lg">Hello, Ana!</div>
-                </div>
-            </div>
+            { subPage === "home" ? null : <FeaturePages data={getData[subPage]} dataName={subPage}/> }
         </>
     );
 };
---
+++ preventiq/src/pages/Home/data.js
@@ -0,0 +1,28 @@
+export const pollenData = [
+    { name: "Birch", value: 120, limit: 50 },
+    { name: "Grass", value: 80, limit: 30 },
+    { name: "Olive", value: 60, limit: 40 },
+    { name: "Ragweed", value: 45, limit: 20 },
+    { name: "Alder", value: 100, limit: 70 },
+    { name: "Mugwort", value: 30, limit: 25 },
+];
+
+export const pollutionData = [
+    { name: "PM2.5", value: 55, limit: 25 },
+    { name: "PM10", value: 90, limit: 50 },
+    { name: "NO₂", value: 40, limit: 40 },
+    { name: "O₃", value: 130, limit: 100 },
+];
+
+export const uvData = [
+    { time: "00:00", value: 0, limit: 5 },
+    { time: "03:00", value: 0, limit: 5 },
+    { time: "06:00", value: 1, limit: 5 },
+    { time: "09:00", value: 3, limit: 5 },
+    { time: "12:00", value: 8, limit: 5 }, // peak
+    { time: "15:00", value: 7, limit: 5 },
+    { time: "18:00", value: 3, limit: 5 },
+    { time: "21:00", value: 1, limit: 5 }
+];
+
+
