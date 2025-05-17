2edf9362d19f7a4052eee1bf11ffed1b5cd84051
AdamDavis08 <hristijangajdov@gmail.com>
2025-05-17 10:12:52 +0200

    Fixed city density and green density

--- convert_bytes_to_image.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-from io import BytesIO
-import numpy as np
-def convert_bytes_to_image(bytes):
-    image = Image.open(BytesIO(bytes)).convert('RGB')
-    image_array = np.array(image)
-    return image_array
